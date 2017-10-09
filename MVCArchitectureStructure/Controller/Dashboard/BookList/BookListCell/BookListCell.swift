6a434d2fda1156807a2e6b275aaa1e897c9cb8c1
Manish Kumar <manish.kumar@innofied.com>
2017-10-09 20:46:46 +0530

    initial commit

---
+++ MVCArchitectureStructure/Controller/Dashboard/BookList/BookListCell/BookListCell.swift
@@ -0,0 +1,9 @@
+//
+//  BookListCell.swift
+//  MVCArchitectureStructure
+//
+//  Created by Manish Kumar on 09/10/17.
+//  Copyright © 2017 Innofied. All rights reserved.
+//
+
+import Foundation
---
+++ MVCArchitectureStructure/Model/Constants/AppFont.swift
@@ -0,0 +1,10 @@
+
+//
+//  AppFont.swift
+//  MVCArchitectureStructure
+//
+//  Created by Manish Kumar on 09/10/17.
+//  Copyright © 2017 Innofied. All rights reserved.
+//
+
+import Foundation
